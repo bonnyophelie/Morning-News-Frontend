6c08a0acd06ef803e1dca5aef42108c70803eb45
Ophélie Bonny <bonny.ophelie@gmail.com>
2024-02-13 17:28:09 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
-#! /usr/bin/env bash
+#! bin/bash
 echo IMAGE="$1" > .env
 
 if [ $1 == "registry.gitlab.com/thedockerdwelers/frontend:pre-prod"]; then
-    docker login registry.gitlab.com -u bonny.ophelie -p glpat-DA2xY5Qbi78Nj1M8WCgv
+    docker login registry.gitlab.com -u bonny.ophelie -p glpat-YHjHmTc3SiVGSXsZGrRM
 else:
     docker login -u bonnyophelie -p dckr_pat_5OPjfn4-CNJFTO9gnD5xH3atUyA
 fi
