43a138d8fa83ff77d86474c06f54a3d5e4a9665d
Ophélie Bonny <bonny.ophelie@gmail.com>
2024-02-14 13:09:27 +0100

    deploy fixed

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
 echo IMAGE="$1" > .env
 
-if [ $1 == "registry.gitlab.com/thedockerdwelers/frontend:pre-prod" ]; then
+if [ "$1" == "registry.gitlab.com/thedockerdwelers/frontend:pre-prod" ]; then
     sudo docker login registry.gitlab.com -u bonny.ophelie -p glpat-YHjHmTc3SiVGSXsZGrRM
 else :
     sudo docker login -u bonnyophelie -p dckr_pat_5OPjfn4-CNJFTO9gnD5xH3atUyA
 fi
 
-if [ sudo docker compose ps -q -gt 0 ]; then
-    sudo docker compose down && sudo docker compose up -d 
+if [ "$(sudo docker compose ps -q)" -gt 0 ]; then
+    sudo docker compose down && sudo docker rmi && sudo docker compose up -d 
 else :
     sudo docker compose up -d
 fi
