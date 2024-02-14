535e8525e3861d9ee832673f573310cc47d592fa
Ophélie Bonny <bonny.ophelie@gmail.com>
2024-02-14 11:20:46 +0100

    fix deploy

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ else :
     sudo docker login -u bonnyophelie -p dckr_pat_5OPjfn4-CNJFTO9gnD5xH3atUyA
 fi
 
-if [ sudo docker compose ps -q > /dev/null 2>&1 ]; then
+if [ sudo docker compose ps -q -gt 0 ]; then
     sudo docker compose down && sudo docker compose up -d 
 else :
     sudo docker compose up -d
