157711f276cea719e15d2e217da7a0f5e0386601
Ophélie Bonny <bonny.ophelie@gmail.com>
2024-02-14 09:10:44 +0100

    add dynamic docker images

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
-echo IMAGE="$1"
+echo IMAGE="$1" > .env
 
 if [ $1 == "registry.gitlab.com/thedockerdwelers/frontend:pre-prod" ]; then
     sudo docker login registry.gitlab.com -u bonny.ophelie -p glpat-YHjHmTc3SiVGSXsZGrRM
-else:
+else :
     sudo docker login -u bonnyophelie -p dckr_pat_5OPjfn4-CNJFTO9gnD5xH3atUyA
 fi
 
