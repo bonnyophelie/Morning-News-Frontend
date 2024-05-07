ebb25d6b615f6193deb7f7af81079182fb32944f
Ophélie Bonny <bonny.ophelie@gmail.com>
2024-05-07 14:03:29 +0200

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,14 @@
 
 echo IMAGE="$1" > .env
 
-if [ "$1" == "$IMAGE_NAME:pre-prod" ]; then
-    sudo docker login $CI_REGISTRY -u $CI_REGISTRY_USER -p $GITLAB_PAT
+if [ "$1" == "*${IMAGE_NAME}:pre-prod" ]; then
+    sudo docker login "$CI_REGISTRY" -u "$CI_REGISTRY_USER" -p "$GITLAB_PAT"
 else :
-    sudo docker login -u $DOCKER_REGISTRY_USER -p $DOCKER_PAT
+    sudo docker login -u "$DOCKER_REGISTRY_USER" -p "$DOCKER_PAT"
 fi
 
 if sudo docker compose ps | grep -q "Up"; then
-    sudo docker compose down && sudo docker rmi $1 && sudo docker compose up -d 
+    sudo docker compose down && sudo docker rmi "$1" && sudo docker compose up -d 
 else :
     sudo docker compose up -d
 fi
